9ed6483a62e7b45a6b36ebd686ce7ec5f4c3e194
Fm_Trick <41456778+Fm-Trick@users.noreply.github.com>
2018-07-19 21:22:56 -0400

    fixed extra tab

--- AutoCanvasSize.lua
+++ AutoCanvasSize.lua
@@ -1,49 +1,49 @@
 local AutoCanvasSize = {}
 
-		-- pass all the GuiObjects inside of the UIGridLayout-organized frame. (Direct children)
-		local function GetHorizontalAmount(objs)
-			local horizontal_amount = -1
-			local absolutes = {}
-			
-			for i=1, #objs do -- loop thru all the frames
-				local o = objs[i]
-				local absolute_y = o.AbsolutePosition.Y
-				if absolutes[absolute_y] then -- if this absolute y exists in the table, add this frame
-					table.insert(absolutes[absolute_y], o) 
-				else
-					absolutes[absolute_y] = {} -- if it doesn't, create it.
-					table.insert(absolutes[absolute_y], o)  -- then add this frame.
-				end
-			end
-			for absolute_y, gui_objs in pairs(absolutes) do -- loop thru the absolutes
-				if #gui_objs > horizontal_amount then -- if this absolute_y has more frames than the current horizontal_amount then, update the horizontal amount
-					horizontal_amount = #gui_objs
-				end
-			end
-			return horizontal_amount -- return the current horizontal amount.
-		end
+	-- pass all the GuiObjects inside of the UIGridLayout-organized frame. (Direct children)
+	local function GetHorizontalAmount(objs)
+		local horizontal_amount = -1
+		local absolutes = {}
 		
-		local function GetVerticalAmount(objs)	
-			local amount = -1
-			local absolutes = {}
-			
-			for i=1, #objs do -- loop thru all the frames
-				local o = objs[i]
-				local absolute_x = o.AbsolutePosition.X
-				if absolutes[absolute_x] then -- if this absolute y exists in the table, add this frame
-					table.insert(absolutes[absolute_x], o) 
-				else
-					absolutes[absolute_x] = {} -- if it doesn't, create it.
-					table.insert(absolutes[absolute_x], o)  -- then add this frame.
-				end
+		for i=1, #objs do -- loop thru all the frames
+			local o = objs[i]
+			local absolute_y = o.AbsolutePosition.Y
+			if absolutes[absolute_y] then -- if this absolute y exists in the table, add this frame
+				table.insert(absolutes[absolute_y], o) 
+			else
+				absolutes[absolute_y] = {} -- if it doesn't, create it.
+				table.insert(absolutes[absolute_y], o)  -- then add this frame.
 			end
-			for absolute_x, gui_objs in pairs(absolutes) do -- loop thru the absolutes
-				if #gui_objs > amount then -- if this absolute_y has more frames than the current amount then, update the horizontal amount
-					amount = #gui_objs
-				end
-			end
-			return amount -- return the current horizontal amount.
 		end
+		for absolute_y, gui_objs in pairs(absolutes) do -- loop thru the absolutes
+			if #gui_objs > horizontal_amount then -- if this absolute_y has more frames than the current horizontal_amount then, update the horizontal amount
+				horizontal_amount = #gui_objs
+			end
+		end
+		return horizontal_amount -- return the current horizontal amount.
+	end
+	
+	local function GetVerticalAmount(objs)	
+		local amount = -1
+		local absolutes = {}
+		
+		for i=1, #objs do -- loop thru all the frames
+			local o = objs[i]
+			local absolute_x = o.AbsolutePosition.X
+			if absolutes[absolute_x] then -- if this absolute y exists in the table, add this frame
+				table.insert(absolutes[absolute_x], o) 
+			else
+				absolutes[absolute_x] = {} -- if it doesn't, create it.
+				table.insert(absolutes[absolute_x], o)  -- then add this frame.
+			end
+		end
+		for absolute_x, gui_objs in pairs(absolutes) do -- loop thru the absolutes
+			if #gui_objs > amount then -- if this absolute_y has more frames than the current amount then, update the horizontal amount
+				amount = #gui_objs
+			end
+		end
+		return amount -- return the current horizontal amount.
+	end
 
 	function AutoCanvasSize.Init(scroller)
 		local grid = scroller:WaitForChild("UIGridLayout")
